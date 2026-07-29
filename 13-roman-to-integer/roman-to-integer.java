b33f649e4eca8fea98c8cf77b3b5233c63328b02
Saeesh Sandip Vele <velesaeesh@gmail.com>
2026-07-29 08:31:33 +0530

    Time: 4 ms (55.02%) | Memory: 46.7 MB (28.62%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -1,74 +1,37 @@
+import java.util.HashMap;
+
 class Solution {
     public int romanToInt(String s) {
+
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+
         int result = 0;
-        for(int i = 0 ; i<s.length() ; i++){
-            char ch = s.charAt(i);
-            switch(ch){
-                case 'I':
-                    if(i+1 < s.length()){
-                        if(s.charAt(i+1)=='V'){
-                            result += 4;
-                            i++;
-                        }
-                        else if(s.charAt(i+1)=='X'){
-                            result += 9;
-                            i++;
-                        }
-                        else{
-                            result += 1;
-                        }
-                    }else{
-                        result += 1;
-                    }
-                    break;
-                case 'X':
-                    if(i+1 < s.length()){
-                        if(s.charAt(i+1)=='L'){
-                            result += 40;
-                            i++;
-                        }
-                        else if(s.charAt(i+1)=='C'){
-                            result += 90;
-                            i++;
-                        }
-                        else{
-                            result += 10;
-                        }
-                    }else{
-                        result += 10;
-                    }
-                    break;
-                case 'C':
-                    if(i+1 < s.length()){
-                        if(s.charAt(i+1)=='D'){
-                            result += 400;
-                            i++;
-                        }
-                        else if(s.charAt(i+1)=='M'){
-                            result += 900;
-                            i++;
-                        }
-                        else{
-                            result += 100;
-                        }
-                    }else{
-                        result += 100;
-                    }
-                    break;
-                case 'V':
-                    result += 5;
-                    break;
-                case 'L':
-                    result += 50;
-                    break;
-                case 'D':
-                    result += 500;
-                    break;
-                case 'M':
-                    result += 1000;
-                    break; 
+
+        for (int i = 0; i < s.length(); i++) {
+
+            int current = map.get(s.charAt(i));
+
+            if (i + 1 < s.length()) {
+                int next = map.get(s.charAt(i + 1));
+
+                if (current < next) {
+                    result -= current;
+                } else {
+                    result += current;
+                }
+            } else {
+                result += current;
             }
         }
-        return result ;
+
+        return result;
     }
 }
